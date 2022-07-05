646963eb180e94939b22f839c3cfdcadc4b00429
JoeIt <josignaciot@gmail.com>
2022-07-04 22:13:55 -0300

    Create Notes Component

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,3 +1,4 @@
+import { Button, Card } from 'react-bootstrap';
 import { Note } from '../models/note.model';
 
 interface INotesProps {
@@ -5,7 +6,20 @@ interface INotesProps {
 }
 
 const Notes: React.FC<INotesProps> = ({ note }) => {
-  return <div>Note Card</div>;
+  return (
+    <div className="mb-3">
+      <Card style={{ backgroundColor: note.color }}>
+        <Card.Body>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Text>{note.text}</Card.Text>
+          <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+          <Button className="mt-3" variant="danger">
+            Delete
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Notes;
