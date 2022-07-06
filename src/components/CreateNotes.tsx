3016533645a9d778fe3a4db8a9bdb0e8f435b36d
JoeIt <josignaciot@gmail.com>
2022-07-06 16:03:16 -0300

    Using Typescript with Refs

---
+++ src/components/CreateNotes.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+interface ICreateNotesProps {}
+
+const CreateNotes: React.FC<ICreateNotesProps> = props => {
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+  const colorRef = useRef<HTMLInputElement | null>(null);
+
+  return (
+    <>
+      <h2>Create Notes</h2>
+      <Form className="mt-3 mb-3">
+        <Form.Group className="mb-3" controlId="formBasicTitle">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Title for the Note"
+            ref={titleRef}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label>Text</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Enter Title for the Note"
+            ref={textRef}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicTitle">
+          <Form.Label htmlFor="colorInput">Notes Color</Form.Label>
+          <Form.Control
+            type="color"
+            id="colorInput"
+            defaultValue="#dfdfdf"
+            title="Choose your color"
+            ref={colorRef}
+          />
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default CreateNotes;
